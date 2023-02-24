1cb21937f58455d30a937527f35216c4cb22c175
Ubaidullah <121395297+UbaidullahTech@users.noreply.github.com>
2023-02-24 04:15:39 -0800

    Remove app.css and index.css

--- redux/src/App.jsx
+++ redux/src/App.jsx
@@ -1,37 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import React from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <h1>{count}</h1>
-        <button onClick={() => setCount((count) => count + 5)}>
-          Add 5
-        </button>
-        <button style={{padding: "10"}} onClick={() => setCount((count) => count - 5)}>
-          Subtract 5
-        </button>
-
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more Ubaidullah
-      </p>
+      <h1>Hello</h1>
     </div>
   )
 }
